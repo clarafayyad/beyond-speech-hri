2fd3e5266dcac79ea09c39332d41771801be19fb
Clara Fayyad <clarafayad@live.com>
2026-02-26 13:09:05 +0100

    Feat: control game state through a UI

--- interaction/game_state.py
+++ interaction/game_state.py
@@ -1,8 +1,9 @@
 
 class GameState:
-    def __init__(self, board, card_descriptions):
+    def __init__(self, board, board_image, card_descriptions):
         self.board = board                      # list of card filenames
         self.card_descriptions = card_descriptions
+        self.board_image = board_image
         self.revealed = {}                      # idx -> blue | red | neutral | assassin
         self.history = []                       # structured guess history
         self.turn = 0
@@ -11,3 +12,29 @@ class GameState:
 
     def is_revealed(self, idx):
         return idx in self.revealed
+
+    def reveal_card(self, idx, team):
+        """
+        team: 'red' | 'blue' | 'neutral' | 'assassin'
+        """
+        if self.game_over:
+            return False
+
+        if not self.is_valid_team(team):
+            return False
+
+        self.revealed[idx] = team
+        print(f"[GAMESTATE] Card {idx} revealed as {team}")
+        return True
+
+    @staticmethod
+    def is_valid_team(team):
+        return team in {'red', 'blue', 'assassin', 'neutral'}
+
+    def unreveal_card(self, idx):
+        if idx in self.revealed:
+            prev = self.revealed[idx]
+            del self.revealed[idx]
+            print(f"[GAMESTATE] Card {idx} unrevealed (was {prev})")
+            return True
+        return False
--- src/main.py
+++ src/main.py
@@ -9,22 +9,26 @@ from codenames.game import CodenamesGame
 from interaction.game_state import GameState
 from interaction.game_loop import GameLoop
 from agents.guesser import Guesser
+from interaction.game_state_server import start_game_state_server
 
 # Configurations & Conversational Agent Setup
-device_manager = Pepper(ip='10.0.0.168')
+# device_manager = Pepper(ip='10.0.0.168')
 # device_manager = Desktop(speakers_conf=SpeakersConf(sample_rate=22050))
 tts_conf = ElevenLabsTTSConf(voice_id='yO6w2xlECAQRFP6pX7Hw', stability=0.2)
-guesser = Guesser(device_manager, tts_conf)
+# guesser = Guesser(device_manager, tts_conf)
+guesser = None
 
 # Build Game
 game = CodenamesGame()
 game_state = GameState(
     board=game.board,
+    board_image=game.board_image,
     card_descriptions=json.load(open("../assets/card_descriptions.json"))
 )
 
-input("Press Enter to start the game")
+start_game_state_server(game_state)
 
+input("Press Enter to start the game")
 
 # Start the interaction
 loop = GameLoop(guesser, game_state)
