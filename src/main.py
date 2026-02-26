171ecc8359064c77bfeab78f09e6b91d30a09249
Clara Fayyad <clarafayad@live.com>
2026-02-26 16:41:47 +0100

    Fix: use new pepper

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ warnings.filterwarnings("ignore")
 
 
 # Configurations & Conversational Agent Setup
-# device_manager = Pepper(ip='10.0.0')
-device_manager = Desktop(speakers_conf=SpeakersConf(sample_rate=22050))
+device_manager = Pepper(ip='10.0.0.148')
+# device_manager = Desktop(speakers_conf=SpeakersConf(sample_rate=22050))
 tts_conf = ElevenLabsTTSConf(voice_id='yO6w2xlECAQRFP6pX7Hw', stability=0.8)
 guesser = Guesser(device_manager, tts_conf)
 
