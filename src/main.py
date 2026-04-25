505c6be6e29723881d06adf9e98a6f63e9b0e2a4
Clara Fayyad <clarafayad@live.com>
2026-04-25 13:18:24 +0200

    fixes & tweaks

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ from agents.guesser import Guesser
 # Configurations
 participant_id = "1"
 is_adaptive = True
+board_config_number = 15
+robot_ip = '10.0.0.148'
+use_robot = True  # Set to False if you want to run without the robot (using desktop audio instead)
 stt_mic_device_index = 4  # This should be the robot's microphone (or desktop mic if not using the robot)
 audio_features_mic_device_index = 1  # This should be the external mic that the participant is wearing
-board_config_number = 1
-robot_ip = '10.0.0.148'
-use_robot = False  # Set to False if you want to run without the robot (using desktop audio instead)
 
 
 def run():
@@ -54,6 +54,7 @@ def run():
     loop.play()
 
     # Shutdown
+    game.shutdown()
     guesser.shutdown()
 
 
