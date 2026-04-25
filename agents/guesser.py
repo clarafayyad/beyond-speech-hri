e26956a3e79f3f35b61becfbe9d0c46c3a449371
Clara Fayyad <clarafayad@live.com>
2026-04-25 10:26:17 +0200

    Add animations

--- agents/guesser.py
+++ agents/guesser.py
@@ -65,6 +65,7 @@ class Guesser:
         return self.llm_agent.prompt_llm(system_prompt, user_prompt)
 
     def say(self, text, sleep_time=0, always_regenerate=False):
+        self.dialog_manager.animate_random()
         self.dialog_manager.say(text, always_regenerate=always_regenerate, sleep_time=sleep_time)
         if isinstance(self.dialog_manager.device_manager, Desktop):
             time.sleep(2)  # To avoid hearing its own speech as feedback
